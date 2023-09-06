e1ecac1b5611740b36c43695833f959fb6a96130
spirteque <xyoh.sb@gmail.com>
2023-09-06 22:56:37 +0200

    feat: login implementation

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,12 @@
-# from django.urls import path
-# from main import views
+from django.urls import path
+from django.contrib.auth import views as auth_views
 
-# namespace = 'account'
+from .forms import UserLoginForm
+from . import views
 
-# urlpatterns = [
-#     path('', ),
-# ]
+namespace = 'account'
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(authentication_form=UserLoginForm), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+]
--- account/views.py
+++ account/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
 
 
 # def login(request):
 #     return render(request, "account/login.html")
+
+
+
+
+
+
+
+
+
+
 
 # def register(request):
 #     return render(request, "account/register.html")
