4a972539b8c4593c27ab626e72596f090262300e
spirteque <xyoh.sb@gmail.com>
2023-09-24 18:54:47 +0200

    chore: follow logic improvements in profile template

--- account/views.py
+++ account/views.py
@@ -110,5 +110,6 @@ def user_profile(request, username):
     user = get_object_or_404(User,
                              username=username,
                              is_active=True)
+
     return render(request, 'account/user/profile.html', {'user': user})
     
