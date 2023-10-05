34edcef4aa0b06afd74cf049456ab89ae0c89ab4
spirteque <xyoh.sb@gmail.com>
2023-10-05 22:17:31 +0200

    chore: timesince adjustments

--- musicary/settings.py
+++ musicary/settings.py
@@ -120,13 +120,11 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
-
-TIME_ZONE = "UTC"
-
+LANGUAGE_CODE = "pl"
+TIME_ZONE = "Europe/Warsaw"
 USE_I18N = True
-
-USE_TZ = True
+USE_L10N = True 
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
