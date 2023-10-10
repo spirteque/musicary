7362f61e973e329376819dece6818b1a8d74b00c
spirteque <xyoh.sb@gmail.com>
2023-10-10 23:45:35 +0200

    chore: changes before publication

--- musicary/settings/base.py
+++ musicary/settings/base.py
@@ -122,7 +122,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = "en" #TODO
+LANGUAGE_CODE = "pl"
 TIME_ZONE = "Europe/Warsaw"
 USE_I18N = True
 USE_L10N = True 
