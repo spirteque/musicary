c3d59f935bf27a41691356a758691a29e503047e
spirteque <xyoh.sb@gmail.com>
2023-10-12 01:53:25 +0200

    fix: profile picture (template)+ new username (view) on settings page

--- account/views.py
+++ account/views.py
@@ -107,23 +107,29 @@ def dashboard(request, action=None):
 
 @login_required
 def edit(request):
+    user_form = UserEditForm(instance=request.user)
+    profile_form = ProfileEditForm(instance=request.user.profile)
+    
     if request.method == 'POST':
-        user_form = UserEditForm(instance=request.user,
-                                 data=request.POST)
-        profile_form = ProfileEditForm(instance=request.user.profile,
-                                       data=request.POST,
-                                       files=request.FILES)
-        if user_form.is_valid() and profile_form.is_valid():
-            user_form.save()
-            profile_form.save()
-            messages.success(request, 'Uaktualnienie profilu zakończyło się sukcesem.')
+        users_with_new_username = User.objects.filter(username = request.POST['username'])
+        
+        if not users_with_new_username:
+            user_form = UserEditForm(instance=request.user,
+                                     data=request.POST)
+            profile_form = ProfileEditForm(instance=request.user.profile,
+                                           data=request.POST,
+                                           files=request.FILES)
+            if user_form.is_valid() and profile_form.is_valid():
+                user_form.save()
+                profile_form.save()
+                messages.success(request, 'Uaktualnienie profilu zakończyło się sukcesem.')
+            else:
+                messages.error(request, 'Wystąpił błąd podczas uaktualniania profilu.')
         else:
-            messages.error(request, 'Wystąpił błąd podczas uaktualniania profilu.')
-    else:
-        user_form = UserEditForm(instance=request.user)
-        profile_form = ProfileEditForm(instance=request.user.profile)
+            messages.error(request, 'Podana nazwa użytkownika już istnieje.')
+            
     return render(request, 'account/edit/edit_account.html', {'user_form': user_form,
-                                                         'profile_form': profile_form})
+                                                              'profile_form': profile_form})
     
 
 class UserPasswordChangeView(SuccessMessageMixin, PasswordChangeView):
