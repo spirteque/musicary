e2759ed802d7e4d8ece3842507e07262448cad44
spirteque <xyoh.sb@gmail.com>
2023-10-12 22:31:14 +0200

    fix: e-mail for password reset

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
+from django.conf import settings
 from django.contrib.auth import views as auth_views
-from django.views.generic import TemplateView
 
-from .forms import UserLoginForm, UserSetNewPasswordForm, UserPasswordResetForm, UserPasswordChangeForm
+from .forms import UserLoginForm, UserSetNewPasswordForm, UserPasswordResetForm
 from . import views
 
 namespace = 'account'
@@ -14,7 +14,8 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('activate/<uidb64>/<token>/', views.activate, name='activate'),
         
-    path('password_reset/', auth_views.PasswordResetView.as_view(form_class=UserPasswordResetForm), name='password_reset'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(form_class=UserPasswordResetForm,
+                                                                 from_email=settings.EMAIL_SENDER), name='password_reset'),
     path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(form_class=UserSetNewPasswordForm), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
