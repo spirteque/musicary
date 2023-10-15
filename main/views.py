5cd260796cb78a71e190f2a709d29dd4689dacf9
spirteque <xyoh.sb@gmail.com>
2023-10-15 13:30:55 +0200

    fix: search results + additional logging

--- main/views.py
+++ main/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from django.contrib.postgres.search import TrigramSimilarity, SearchVector
+from django.contrib.postgres.search import TrigramSimilarity
 from .forms import SearchForm
 from posts.models import Post
 import logging
+
+def find_by_similarity(model, by, query, limit=18, similarity_qt=0.1, order_by='-similarity', logger=None):
+    results =  model.objects.annotate(similarity=TrigramSimilarity(by, query),).filter(similarity__gt=similarity_qt).order_by(order_by)[:limit]
+    
+    if logger:
+         logger.info(msg=f'Search result in={model.__name__}; by={by}; with query={query}; results={list(results)}')
+         
+    return results
+    
 
 logger = logging.getLogger(__name__)
 
@@ -24,27 +33,21 @@ def search(request, action=None):
         
         if form.is_valid():
             query = form.cleaned_data['query']
-            logger.info(msg=f'Search by query={query} for user with id={request.user.id}')
             
         
     if action == 'users':
-        user_results = User.objects.annotate(similarity=TrigramSimilarity('username', query),
-                                             ).filter(similarity__gt=0.1).order_by('-similarity')[:18]
-        user_tag_results = Post.objects.annotate(similarity=TrigramSimilarity('friend_tags__username', query),
-                                                ).filter(similarity__gt=0.1).order_by('-similarity')[:18]
+        user_results = find_by_similarity(User, 'username', query, logger=logger)
+        user_tag_results = find_by_similarity(Post, 'friend_tags__username', query, logger=logger)
         
         return render(request, 'main/search_users.html', {'query': query,
                                                           'user_results': user_results,
                                                           'user_tag_results': user_tag_results})
     if action == 'posts':
-        post_results = Post.objects.annotate(similarity=TrigramSimilarity('title', query),
-                                             ).filter(similarity__gt=0.1).order_by('-similarity')[:9]
-        artist_results = Post.objects.annotate(search=SearchVector('artists'),
-                                              ).filter(search=query)[:9]
-        mood_tag_results = Post.objects.annotate(search=SearchVector('author_tags'),
-                                                 ).filter(search=query)[:9]
-        genre_results = Post.objects.annotate(search=SearchVector('genre'),
-                                              ).filter(search=query)[:9]
+        post_results = find_by_similarity(Post, 'title', query, logger=logger, limit=9)
+        artist_results = find_by_similarity(Post, 'artists', query, logger=logger, limit=9)
+        mood_tag_results = find_by_similarity(Post, 'author_tags', query, logger=logger, limit=9)
+        genre_results = find_by_similarity(Post, 'genre', query, logger=logger, limit=9)
+        
         return render(request, 'main/search_posts.html', {'query': query,
                                                           'post_results': post_results,
                                                           'artist_results': artist_results,
